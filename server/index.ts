f7a7924a9a816cd7234cc83e43dba75d75f27f30
= <saumyabshah90@gmail.com>
2024-08-04 21:57:47 +0530

    port configs and test ansible config workflows

--- server/index.ts
+++ server/index.ts
@@ -1,68 +1,84 @@
-import express from 'express';
-import http from 'http';
-import { Server } from 'socket.io';
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
 
 const app = express();
 const server = http.createServer(app);
 
-import { leaveRoom } from './controllers/leaveRoom';
-import { DrawLine } from './types/types';
+import { leaveRoom } from "./controllers/leaveRoom";
+import { DrawLine } from "./types/types";
 
 const io = new Server(server, {
-    cors: {
-        origin: ["http://localhost:3000", "https://doodler-ecru.vercel.app", "https://doodler-saumya40-codes.vercel.app/"],
-        methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  cors: {
+    origin: [
+      "http://localhost:3000",
+      "https://doodler-ecru.vercel.app",
+      "https://doodler-saumya40-codes.vercel.app/",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  },
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).send("ok");
+});
+
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
+  console.log("listening on port 5000");
+});
+
+io.on("connection", (socket) => {
+  socket.on("new-client", (roomId: string) => {
+    io.to(roomId).emit("get-canvas-state");
+  });
+
+  socket.on("join-room", (roomId: string, username: string) => {
+    socket.join(roomId);
+    socket.handshake.query = { roomId, username };
+
+    io.to(roomId).emit("user-connected", username);
+  });
+
+  socket.on(
+    "draw-line",
+    ({ prevPoint, currentPoint, color, roomId }: DrawLine) => {
+      socket.broadcast
+        .to(roomId)
+        .emit("draw-line", { prevPoint, currentPoint, color });
+    },
+  );
+
+  socket.on("canvas-state", ({ canvasState, roomId }) => {
+    io.to(roomId).emit("canvas-state-from-server", canvasState);
+  });
+
+  socket.on("clear", (roomId: string) => {
+    io.to(roomId).emit("clear");
+  });
+
+  socket.on("disconnect", async () => {
+    const { roomId, username } = socket.handshake.query as {
+      roomId: string;
+      username: string;
+    };
+
+    try {
+      if (roomId && username) {
+        await leaveRoom(roomId, username);
+
+        socket.broadcast.to(roomId).emit("user-disconnected", username);
+      }
+    } catch (error) {
+      console.error(error);
     }
-});
+  });
 
-app.get('/health', (req, res) => {
-  res.status(200).send('ok');
-});
-
-server.listen(5000, () => {
-    console.log('listening on port 5000');
-});
-
-io.on('connection', (socket) => {
-
-    socket.on('new-client', (roomId: string) => {
-        io.to(roomId).emit('get-canvas-state');
-    });
-
-    socket.on('join-room', (roomId: string, username: string) => {
-        socket.join(roomId);
-        socket.handshake.query = { roomId, username };
-
-        io.to(roomId).emit('user-connected', username);
-    });
-
-    socket.on('draw-line', ({ prevPoint, currentPoint, color, roomId }: DrawLine) => {
-        socket.broadcast.to(roomId).emit('draw-line', { prevPoint, currentPoint, color });
-    });
-
-    socket.on('canvas-state', ({canvasState,roomId}) => {
-        io.to(roomId).emit('canvas-state-from-server', canvasState);
-    });
-
-    socket.on('clear', (roomId: string) => {
-        io.to(roomId).emit('clear');
-    });
-
-    socket.on('disconnect', async () => {
-        const { roomId, username } = socket.handshake.query as { roomId: string, username: string };
-    
-        try {
-            if (roomId && username) {
-                await leaveRoom(roomId, username);
-    
-                socket.broadcast.to(roomId).emit('user-disconnected', username);
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    });
-
-    socket.on('message', (message: {message:string,roomId:string,username:string}) => {
-        socket.broadcast.to(message.roomId).emit('message-receive', message);
-    });
+  socket.on(
+    "message",
+    (message: { message: string; roomId: string; username: string }) => {
+      socket.broadcast.to(message.roomId).emit("message-receive", message);
+    },
+  );
 });
